7cbde6cb8c35776831a7c2fb92133164a663c640
Kris Evers <krisevers14@gmail.com>
2025-08-26 15:34:09 +0200

    add publications

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,16 @@
 rm -rf public/*
 zine release
 
-# deploy
+# push to main branch
+if [ "$1" = "-m" ] && [ -n "$2" ]; then
+    message="$2"
+else
+    message="auto commit"
+fi
+git add *
+git commit -m "$message"
+git push
+
+# deploy to gh_pages branch
 ssh -T git@github.com
 git subtree push --prefix public origin gh-pages
